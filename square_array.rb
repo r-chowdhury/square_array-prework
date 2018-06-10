0ee6059e829bb0df2f1ce76019e507fc4b1788a2
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-10 18:34:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   array.each do |number|
     squared_number = number ** 2
-    new_array.push(number)
+    new_array.push(squared_number)
   end
   return new_array
 end
